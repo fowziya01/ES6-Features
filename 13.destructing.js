5907a29d8c5fbfe23c676c0cfc189e3c83a8d8a0
fowziya01 <fowzijunaid@gmail.com>
2024-12-09 20:31:34 +0530

    Mutilevel Array and object destructing

--- 13.destructing.js
+++ 13.destructing.js
@@ -2,10 +2,15 @@ const people = [ {
     name: "Alice", 
     address: 
     { city: "New York", 
-    street: { name: "Broadway", number: 123 } } },
+    street: 
+    { name: "Broadway", number: 123 } } },
      { name: "Bob", 
         address: 
-        { city: "Los Angeles",
-     street: { name: "Sunset Boulevard", number: 456 } } } ];
-     
+        { city:"Los Angeles",
+     street: 
+     { name: "Sunset Boulevard", number: 456 } } } ];
+     const result=people.map(({name:myName,address: {city:myCity="Unknown",street:{name:streetName="No street"}={}}={}})=>{
+      return (`"${myName} lives in ${myCity} on ${streetName}"`);
+     });
 
+     console.log(result);
